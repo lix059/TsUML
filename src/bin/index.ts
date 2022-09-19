2a3a9cbfdb5ee1facae9d8898c80961713f65e27
xiong.li@shopee.com <xiong.li@shopee.com>
2022-09-19 09:53:37 +0800

    feat: support plantUML

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -5,25 +5,29 @@ import * as yargs from "yargs";
 import { getUrl } from "../core";
 
 (async () => {
-
-    try {
-
-        if (yargs.argv.help) {
-            console.log(chalk.yellowBright("tsuml --glob ./src/**/*.ts"));
-        }
-
-        const pattern = yargs.argv.glob;
-
-        if (!pattern) {
-            console.log(chalk.redBright("Missing --glob"));
-        } else {
-            const url = await getUrl("./tsconfig.json", pattern);
-            const opn = require("opn");
-            opn(url);
-        }
-
-    } catch(e) {
-        console.log(chalk.redBright(e));
+  try {
+    if (yargs.argv.help) {
+      console.log(chalk.yellowBright("tsuml --glob ./src/**/*.ts"));
     }
 
+    const pattern = yargs.argv.glob;
+
+    if (!pattern) {
+      console.log(chalk.redBright("Missing --glob"));
+    } else {
+      // const url = await getUrl(
+      //   "/Users/xiong.li/Desktop/work/credit_fronted/risk_admin/credit_risk_admin_node/tsconfig.json",
+      //   "/Users/xiong.li/Desktop/work/credit_fronted/risk_admin/credit_risk_admin_node/src/plugins/**/*.ts"
+      // );
+      const url = await getUrl(
+        "/Users/xiong.li/Desktop/work/credit_fronted/risk_admin/credit_risk_admin/tsconfig.json",
+        "/Users/xiong.li/Desktop/work/credit_fronted/risk_admin/credit_risk_admin/src/components/bpmn/**/*.*"
+      );
+      console.log(url);
+      const opn = require("opn");
+      opn(url);
+    }
+  } catch (e) {
+    console.log(chalk.redBright(e));
+  }
 })();
